f24f979833fcafeb72c9518c138a2384b99aa460
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-07-21 21:00:05 +0200

    node-schedule erabiliz egunero 9:10 eta 21:10etan txiokatzeko programatu

--- index.js
+++ index.js
@@ -1,66 +1,74 @@
 var request = require("request");
 var Twit = require('twit');
 var fs = require('fs');
+var schedule = require('node-schedule');
 
 var config = JSON.parse(fs.readFileSync('./config.json'));
 
-var T = new Twit({
-    consumer_key:         config.twitter_app.consumer_key,
-    consumer_secret:      config.twitter_app.consumer_secret,
-    access_token:         config.twitter_app.access_token,
-    access_token_secret:  config.twitter_app.access_token_secret
+var j = schedule.scheduleJob('10 9,21 * * *', function() {
+    txiokatuWikidosia();
 });
 
-var emaitza = {
-    pageid: undefined,
-    title: "",
-    extract: ""
-};
+function txiokatuWikidosia() {
 
-var mezua = "";
+    var T = new Twit({
+        consumer_key:         config.twitter_app.consumer_key,
+        consumer_secret:      config.twitter_app.consumer_secret,
+        access_token:         config.twitter_app.access_token,
+        access_token_secret:  config.twitter_app.access_token_secret
+    });
 
-request({
-    url: "http://eu.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&exintro=&format=json",
-    json: true
-}, function (error, response, body) {
+    var emaitza = {
+        pageid: undefined,
+        title: "",
+        extract: ""
+    };
 
-    if (!error && response.statusCode === 200) {
+    var mezua = "";
 
-        for (var orria in body.query.pages) {
+    request({
+        url: "http://eu.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&exintro=&format=json",
+        json: true
+    }, function (error, response, body) {
 
-            emaitza.pageid = body.query.pages[orria].pageid;
-            emaitza.title = body.query.pages[orria].title;
-            emaitza.extract = body.query.pages[orria].extract;
+        if (!error && response.statusCode === 200) {
 
-            request({
-                url: "http://eu.wikipedia.org/w/api.php?action=query&prop=info&pageids=" + emaitza.pageid + "&inprop=url&format=json",
-                json: true
-            }, function(error, response, body) {
+            for (var orria in body.query.pages) {
 
-                if (!error && response.statusCode === 200) {
+                emaitza.pageid = body.query.pages[orria].pageid;
+                emaitza.title = body.query.pages[orria].title;
+                emaitza.extract = body.query.pages[orria].extract;
 
-                    for (var orria in body.query.pages) {
+                request({
+                    url: "http://eu.wikipedia.org/w/api.php?action=query&prop=info&pageids=" + emaitza.pageid + "&inprop=url&format=json",
+                    json: true
+                }, function(error, response, body) {
 
-                        emaitza.fullurl = body.query.pages[orria].fullurl;
+                    if (!error && response.statusCode === 200) {
+
+                        for (var orria in body.query.pages) {
+
+                            emaitza.fullurl = body.query.pages[orria].fullurl;
+
+                        }
+
+                        mezua = emaitza.title + " - " + emaitza.extract;
+
+                        mezua = mezua.substring(0, 140 - 27);
+
+                        mezua = mezua + "... " + emaitza.fullurl;
+
+                        T.post('statuses/update', { status: mezua }, function(err, data, response) {
+                            if(err) {
+                                console.log("There was a problem tweeting the message.", err);
+                            }
+                        });
 
                     }
 
-                    mezua = emaitza.title + " - " + emaitza.extract;
-
-                    mezua = mezua.substring(0, 140 - 27);
-
-                    mezua = mezua + "... " + emaitza.fullurl;
-
-                    T.post('statuses/update', { status: mezua }, function(err, data, response) {
-                        if(err) {
-                            console.log("There was a problem tweeting the message.", err);
-                        }
-                    });
-
-                }
-
-            });
+                });
+            }
         }
-    }
 
-});
+    });
+}
