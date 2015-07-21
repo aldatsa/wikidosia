e78b17a96fdf966e3e15f9fff28928db5a1447f1
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-07-21 23:29:15 +0200

    Berriz ere nire kodea jarri

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 #!/bin/env node
-//  OpenShift sample Node application
+
 var express = require('express');
-var fs = require('fs');
 var request = require("request");
 var Twit = require('twit');
+var fs = require('fs');
 var schedule = require('node-schedule');
 
 /**
@@ -69,7 +69,6 @@ var SampleApp = function() {
         }
         console.log('%s: Node server stopped.', Date(Date.now()) );
     };
-
 
     /**
      *  Setup termination handlers (for exit and a list of signals).
@@ -141,11 +140,82 @@ var SampleApp = function() {
      *  Start the server (starts up the sample application).
      */
     self.start = function() {
+
+        self.config = JSON.parse(fs.readFileSync('./config.json'));
+
+        // EDT ordua (-6 ordu)
+        var j = schedule.scheduleJob('2 5,17 * * *', function() {
+            self.txiokatuWikidosia();
+        });
+
         //  Start the app on the specific interface (and port).
         self.app.listen(self.port, self.ipaddress, function() {
             console.log('%s: Node server started on %s:%d ...',
                         Date(Date.now() ), self.ipaddress, self.port);
-            console.log("Kaixo");
+        });
+    };
+
+    self.txiokatuWikidosia = function() {
+
+        var T = new Twit({
+            consumer_key:         self.config.twitter_app.consumer_key,
+            consumer_secret:      self.config.twitter_app.consumer_secret,
+            access_token:         self.config.twitter_app.access_token,
+            access_token_secret:  self.config.twitter_app.access_token_secret
+        });
+
+        var emaitza = {
+            pageid: undefined,
+            title: "",
+            extract: ""
+        };
+
+        var mezua = "";
+
+        request({
+            url: "http://eu.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&exintro=&format=json",
+            json: true
+        }, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+
+                for (var orria in body.query.pages) {
+
+                    emaitza.pageid = body.query.pages[orria].pageid;
+                    emaitza.title = body.query.pages[orria].title;
+                    emaitza.extract = body.query.pages[orria].extract;
+
+                    request({
+                        url: "http://eu.wikipedia.org/w/api.php?action=query&prop=info&pageids=" + emaitza.pageid + "&inprop=url&format=json",
+                        json: true
+                    }, function(error, response, body) {
+
+                        if (!error && response.statusCode === 200) {
+
+                            for (var orria in body.query.pages) {
+
+                                emaitza.fullurl = body.query.pages[orria].fullurl;
+
+                            }
+
+                            mezua = emaitza.title + " - " + emaitza.extract;
+
+                            mezua = mezua.substring(0, 140 - 27);
+
+                            mezua = mezua + "... " + emaitza.fullurl;
+
+                            T.post('statuses/update', { status: mezua }, function(err, data, response) {
+                                if(err) {
+                                    console.log("There was a problem tweeting the message.", err);
+                                }
+                            });
+
+                        }
+
+                    });
+                }
+            }
+
         });
     };
 
