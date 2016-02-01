85ef51f207ea6adc708d591faed76f9e7f952760
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-02-01 22:46:34 +0100

    Nabarmenduen array-a desordenatuta ere gorde

--- scriptak/gorde_nabarmenduak.js
+++ scriptak/gorde_nabarmenduak.js
@@ -15,14 +15,52 @@ var EuskalWikipedia = require("../lib/euskalwikipedia");
 var euskalwikipedia = new EuskalWikipedia();
 
 var fitxategia = "openshift/nabarmenduak.json";
+var fitxategia_desordenatuta = "openshift/nabarmenduak-desordenatuta.json";
+
+/*
+ * Array bat jaso eta desordenatuta itzultzen du.
+ */
+function desordenatuArraya(array) {
+
+    // Fisher-Yates
+    // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 euskalwikipedia.eskuratuNabarmenduenZerrenda().then(function(emaitza) {
 
+    // Array dagoen bezala gorde, erreferentzia bezala.
     fs.writeFile(fitxategia, JSON.stringify(emaitza, null, 4), function(err) {
         if (err) {
             console.log(err);
         } else {
             console.log("JSON fitxategia behar bezala gorde da: " + fitxategia);
+        }
+    });
+
+    // Array-a ausaz ordenatu.
+    emaitza = desordenatuArraya(emaitza);
+
+    fs.writeFile(fitxategia_desordenatuta, JSON.stringify(emaitza, null, 4), function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("JSON fitxategia behar bezala gorde da: " + fitxategia_desordenatuta);
         }
     });
 
